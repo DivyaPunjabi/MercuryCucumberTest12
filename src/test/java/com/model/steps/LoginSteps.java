363b77255f2d5e2ca2889f4c61cd26ed1a19f910
DivyaPunjabi <52617986+DivyaPunjabi@users.noreply.github.com>
2019-07-08 13:17:30 +0530

    Update LoginSteps.java

--- src/test/java/com/model/steps/LoginSteps.java
+++ src/test/java/com/model/steps/LoginSteps.java
@@ -42,6 +42,7 @@ public class LoginSteps
 	public void enterApplicationUrl(String url)
 	{
 		Config.driver.get(url);
+		
 	}
 	
 	@And ("^user waits \"(.*)\" seconds to Login page to be loaded$")
@@ -74,12 +75,14 @@ public class LoginSteps
 	public void enterUserName(String username)
 	{
 		Config.driver.findElement(By.xpath("//input[@name='userName']")).sendKeys(username);
+		System.out.println("Username entered successfully");
 	}
 	
 	@And ("^user enters \"(.*)\" as password$")
 	public void enterPassword(String password)
 	{
 		Config.driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+		System.out.println("Password entered successfully");
 	}
 	
 	@And ("^user clicks on Sign In button$")
